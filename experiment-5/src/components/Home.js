9c3da349c0b3cff0eaecb45858032e4efe911c53
Piyush Kakkar <piyushkakkar587@gmail.com>
2026-02-05 10:33:43 +0530

    experiment-5

---
+++ experiment-5/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="card">
+      <h2>About Component</h2>
+      <p>
+        This component is loaded lazily only when the button is clicked.
+      </p>
+    </div>
+  );
+}
+
+export default About;
