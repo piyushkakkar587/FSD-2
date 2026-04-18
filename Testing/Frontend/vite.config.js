38dbaadb36c671fed592b33319852262e8227be8
Piyush Kakkar <piyushkakkar587@gmail.com>
2026-04-18 14:23:36 +0530

    Frontend

--- Testing/Frontend/vite.config.js
+++ Testing/Frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: './src/setupTests.js'   // ✅ correct path
+    setupFiles: './src/setupTests.js'
   }
 })
